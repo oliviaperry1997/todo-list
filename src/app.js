7521792a9c78acc1ffc70b634852f5f4af40391b
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-05-26 12:49:28 +0100

    Add logic for creating new projects
    This commit adds logic for creating new projects, which will serve as
    folders for to-do items.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-export function createTodo(title, description, dueDate, priority, notes) {
+export let projectList = [];
+export function createProject(name) {
+    projectList.push(name);
+    return {name};
+};
+export function createTodo(title, description, dueDate, priority, notes, project) {
+    if (project = "") {
+        project = "default";
+    }
     return {title, description, dueDate, priority, notes}
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import { createTodo } from "./app";
+import { createProject } from "./app";
+import { projectList } from "./app";
 
 const newTodoBtn = document.createElement("button");
 newTodoBtn.textContent = "New Todo";
 document.body.appendChild(newTodoBtn);
 
+const newProjectBtn = document.createElement("button");
+newProjectBtn.textContent = "New Project";
+document.body.appendChild(newProjectBtn);
+
+newProjectBtn.addEventListener("click", () => {
+    const newProjectForm = document.createElement("dialog");
+    newProjectForm.innerHTML = "<form><label for='name'>Name:</label><input id='name' name='name'><button id='project-submit' type='button'>Submit</button></form>";
+    document.body.appendChild(newProjectForm);
+    newProjectForm.showModal();
+    const newProjectName = document.querySelector("#name");
+    newProjectForm.querySelector("#project-submit").addEventListener("click", () => {
+        const newProject = createProject(newProjectName.value);
+        const projectDiv = document.createElement("div");
+        projectDiv.id = `project-${newProject.name.toLowerCase()}`;
+        projectDiv.innerHTML = `<h2>${newProject.name}</h2>`
+        document.querySelector("#todo-container").appendChild(projectDiv);
+
+        newProjectForm.close();
+    })
+})
+
 newTodoBtn.addEventListener("click", () => {
     const newTodoForm = document.createElement("dialog");
-    newTodoForm.innerHTML = "<form><label for='title'>Title:</label><input id='title' name='title'><label for='desc'>Description:</label><input id='desc' name='desc'><label for='deadline'>Due By:</label><input id='deadline' type='date' name='deadline'><label for='priority'>High Priority?</label><input id='priority' type='checkbox' name='priority'><label for='notes'>Notes:</label><input id='notes' name='notes'><button id='submit' type='button'>Submit</button></form>";
+    newTodoForm.innerHTML = "<form><label for='title'>Title:</label><input id='title' name='title'><label for='desc'>Description:</label><input id='desc' name='desc'><label for='deadline'>Due By:</label><input id='deadline' type='date' name='deadline'><label for='priority'>High Priority?</label><input id='priority' type='checkbox' name='priority'><label for='notes'>Notes:</label><input id='notes' name='notes'><button id='todo-submit' type='button'>Submit</button></form>";
     document.body.appendChild(newTodoForm);
     newTodoForm.showModal();
     const newTodoTitle = document.querySelector("#title");
@@ -14,7 +37,7 @@ newTodoBtn.addEventListener("click", () => {
     const newTodoDeadline = document.querySelector("#deadline");
     const newTodoPriority = document.querySelector("#priority");
     const newTodoNotes = document.querySelector("#notes");
-    newTodoForm.querySelector("#submit").addEventListener("click", () => {
+    newTodoForm.querySelector("#todo-submit").addEventListener("click", () => {
         const newTodo = createTodo(newTodoTitle.value, newTodoDesc.value, newTodoDeadline.value, newTodoPriority.checked, newTodoNotes.value)
         const todoDiv = document.createElement("div");
 
