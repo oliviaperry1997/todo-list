32f277d01b8be2c09bba79ec43c6251173654590
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-05-28 11:43:37 +0100

    Implement localStorage utilization
    This commit stores projectList on localStorage and calls it when
    needed. Since the function loadProjectList is exported as projectList
    from app.js, any function that calls projectList in index.js can remain
    unmodified.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
-export let projectList = [
-    {
-        projectName: "Default",
-        todoList: []
-    }
-];
+export let projectList = loadProjectList();
+
+function loadProjectList() {
+    const data = localStorage.getItem("projectList");
+    return data ? JSON.parse(data) : [
+        {
+            projectName: "Default",
+            todoList: []
+        }
+    ];
+}
+
+function saveProjectList() {
+    localStorage.setItem("projectList", JSON.stringify(projectList));
+}
+
 export function createProject(name) {
     const existing = projectList.find(p => p.projectName.toLowerCase() === name.toLowerCase());
     if (!existing && name.trim()) {
         projectList.push({projectName: name, todoList: []});
+        saveProjectList();
     }
 };
 export function createTodo(title, description, dueDate, priority, notes, project) {
@@ -28,10 +39,11 @@ export function createTodo(title, description, dueDate, priority, notes, project
     if (targetProject) {
         targetProject.todoList.push(todoItem)
     } else {
-        const newProject = {
+        projectList.push({
             projectName: project,
             todoList: [todoItem]
-        };
-        projectList.push(newProject);
+        });
     };
+
+    saveProjectList();
 };
